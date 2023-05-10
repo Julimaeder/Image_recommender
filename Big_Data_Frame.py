ad9bdeda097826dcbcf94b58d09557bba02a7f81
Julimaeder <maeder.julian@gmail.com>
2023-05-11 00:39:21 +0200

    tests

--- Big_Data_Frame.py
+++ Big_Data_Frame.py
@@ -3,3 +3,42 @@
 Big Data Projekt Frame
 """
 
+"""Datenbank"""
+def connector():
+    pass
+
+def WriteDatenbank():
+    pass
+
+def ReadDatenbank():
+    pass
+
+"""Vorbereitung"""
+def GetFarbschema():
+    pass
+
+def GoogleAPI():
+    pass
+
+def VorbereitungCNN():
+    pass
+
+def CNN():
+    pass
+
+"""Programm"""
+def Eingabe():
+    pass
+
+def KNNFarbschema():
+    pass
+
+def KNNAPI():
+    pass
+
+def Predict():
+    pass
+
+def Ausgabe():
+    pass
+
