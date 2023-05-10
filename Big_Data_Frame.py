af7fa568475d74fb25bce396c2cc5b5bb2456857
bro-tec <khan0shazil@gmail.com>
2023-05-11 01:10:25 +0200

    implemented image_to_rgb into Big_Data_Frame

--- Big_Data_Frame.py
+++ Big_Data_Frame.py
@@ -3,6 +3,9 @@
 Big Data Projekt Frame
 """
 
+from PIL import Image
+import os
+
 """Datenbank"""
 def connector():
     pass
@@ -19,6 +22,43 @@ def GetFarbschema():
 
 def GoogleAPI():
     pass
+
+def image_to_rgb():
+    # Set the path to the directory that contains the images
+    directory = "path/to/directory"
+
+    # Loop through each file in the directory
+    def get_pictures(directory):
+        for filename in os.listdir(directory):
+            # Check if the file is an image file
+            if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+                # Open the image file
+                yield Image.open(os.path.join(directory, filename))
+
+    image_generator = get_pictures(directory)
+
+    # Open the image file
+    for image in image_generator:
+        
+        # Convert the image to RGB mode if it's not already in RGB mode
+        image = image.convert("RGB")
+        
+        # Get the width and height of the image
+        width, height = image.size
+        
+        # Create an empty list to store the vectors
+        vectors = []
+        
+        # Loop through each pixel in the image
+        for x in range(width):
+            for y in range(height):
+                # Get the RGB values of the pixel
+                r, g, b = image.getpixel((x, y))
+                # Append the RGB values to the list as a vector
+                vectors.append([r, g, b])
+        
+        # return the list of vectors
+        yield vectors
 
 def VorbereitungCNN():
     pass
