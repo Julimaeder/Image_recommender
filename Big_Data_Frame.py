156303289a3431f3829bc4c2f3819a1b8e7a41d8
bro-tec <ig0brudi@gmail.com>
2023-06-15 02:29:49 +0200

    added a faster way for color scheme and implemented databases

--- Big_Data_Frame.py
+++ Big_Data_Frame.py
@@ -2,17 +2,45 @@
 """
 Big Data Projekt Frame
 """
-
 from PIL import Image
+import numpy as np
 from tqdm import tqdm
 import os
+import matplotlib.pyplot as plt
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Informatiker1",
+  autocommit=True
+)
+
+mycursor = mydb.cursor(buffered=True)
 
 """Database"""
-def Connector():
-    pass
+def CreateDatabase():
+    #just execute it if the Database isn't implemented
+    mycursor.execute("create database bigData")
+    mycursor.execute("create table images(ID varchar2(24) primary key not null, path varchar2(120))")
+    mycursor.execute("create table pixels(ID varchar2(24), row int(100), col int(100), r int(3), g int(3), b int(3), primary key(ID, row, col), foreign key (ID) references images(ID))")
+    mycursor.execute("create table schemes(ID varchar2(24), r int(3), g int(3), b int(3), amount int(10), primary key(ID, r, g, b), foreign key (ID) references images(ID))")
 
-def WriteDatabase():
-    pass
+def LoadDatabase():
+    #just execute it if the Database isn't implemented
+    mycursor.execute("use bigData")
+
+def CheckDatabase():
+    #just execute it if the Database isn't implemented
+    #mycursor.execute(
+    sql_query = "SELECT IF(EXISTS (SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'bigData'), TRUE, FALSE) AS database_exists"
+    result = pd.read_sql(sql_query, con=mydb)
+    print(result)
+    if result:
+        LoadDatabase()
+    else:
+        CreateDatabase()
 
 def ReadDatabase():
     pass
@@ -55,6 +83,50 @@ def Get_color_scheme(vectors):
             count_scheme2[color_scheme2.index(converted_rgb)] += count_value
     return color_scheme2, count_scheme2
 
+def Get_color_scheme_test(vectors):
+    vectors = np.array(vectors)
+    unique_vectors, vector_counts = np.unique(vectors, axis=0, return_counts=True)
+    
+    converted_vectors = np.where((unique_vectors % 5) >= 3, unique_vectors + (5 - (unique_vectors % 5)), unique_vectors - (unique_vectors % 5))
+    
+    unique_converted, converted_counts = np.unique(converted_vectors, axis=0, return_counts=True)
+    
+    return unique_converted.tolist(), converted_counts.tolist()
+
+# def get_color_scheme_numpy(image):
+#     # Get the pixel data from the image
+#     pixels = np.array(image)
+
+#     # Flatten the pixel data to a 2D array of shape (N, 3), where N is the total number of pixels
+#     pixels_flat = pixels.reshape(-1, 3)
+
+#     # Calculate the unique colors and their counts
+#     unique_colors, color_counts = np.unique(pixels_flat, axis=0, return_counts=True)
+    
+#     color_scheme1 = list(unique_colors)
+#     count_scheme1 = list(color_counts)
+#     color_scheme2 = []
+#     count_scheme2 = []
+
+#     # Print the color counts
+#     #for color, count in zip(unique_colors, color_counts):
+#      #   print(f"RGB: {color}, Count: {count}")
+#     for rgb in tqdm(color_scheme1):
+#         count_value = count_scheme1[color_scheme1.index(rgb)]
+#         converted_rgb = []
+#         for value in rgb:
+#             val_rest = value % 5
+#             if val_rest >= 3:
+#                 converted_rgb.append(value + (5 - val_rest)) 
+#             else:
+#                 converted_rgb.append(value - val_rest)
+#         if converted_rgb not in color_scheme2:
+#             color_scheme2.append(converted_rgb)
+#             count_scheme2.append(count_value)
+#         else:
+#             count_scheme2[color_scheme2.index(converted_rgb)] += count_value
+#     return color_scheme2, count_scheme2
+
 def GoogleAPI():
     pass
 
@@ -93,7 +165,11 @@ def Full_Preperation():
     for image in image_generator:
         vectors = Image_to_rgb(image)
         color_scheme, count_scheme = Get_color_scheme(vectors)
-    
+        color_schemet, count_schemet = Get_color_scheme_test(vectors)
+        #color_schemenp, count_schemenp = get_color_scheme_numpy(vectors)
+        print(np.setdiff1d(np.array([color_scheme, count_scheme]), np.array([color_schemet, count_schemet]))) 
+        print("done next")
+        break
 
 """Code"""
 def Input():
@@ -110,3 +186,5 @@ def Predict():
 
 def Output():
     pass
+
+Full_Preperation()
