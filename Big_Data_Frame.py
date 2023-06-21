cfb8306175863b9b65b54ac700dd5f76f3549020
bro-tec <ig0brudi@gmail.com>
2023-06-21 23:01:29 +0200

    added databases and saviung data in it

--- Big_Data_Frame.py
+++ Big_Data_Frame.py
@@ -6,6 +6,7 @@ from PIL import Image
 import numpy as np
 from tqdm import tqdm
 import os
+import pandas as pd
 import matplotlib.pyplot as plt
 import mysql.connector
 
@@ -19,13 +20,16 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor(buffered=True)
 
+
 """Database"""
 def CreateDatabase():
     #just execute it if the Database isn't implemented
     mycursor.execute("create database bigData")
-    mycursor.execute("create table images(ID varchar2(24) primary key not null, path varchar2(120))")
-    mycursor.execute("create table pixels(ID varchar2(24), row int(100), col int(100), r int(3), g int(3), b int(3), primary key(ID, row, col), foreign key (ID) references images(ID))")
-    mycursor.execute("create table schemes(ID varchar2(24), r int(3), g int(3), b int(3), amount int(10), primary key(ID, r, g, b), foreign key (ID) references images(ID))")
+    mycursor.execute("use bigData")
+    mycursor.execute("create table images(iID int primary key not null, ipath varchar(120) unique)")
+    mycursor.execute("create table pixels(pID int, prow int(100), pcol int(100), r int(3), g int(3), b int(3), primary key(pID, prow, pcol), foreign key (pID) references images(iID))")
+    mycursor.execute("create table schemes(sID int, r int(3), g int(3), b int(3), amount int(10), primary key(sID, r, g, b), foreign key (sID) references images(iID))")
+    mydb.commit()
 
 def LoadDatabase():
     #just execute it if the Database isn't implemented
@@ -36,54 +40,46 @@ def CheckDatabase():
     #mycursor.execute(
     sql_query = "SELECT IF(EXISTS (SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'bigData'), TRUE, FALSE) AS database_exists"
     result = pd.read_sql(sql_query, con=mydb)
-    print(result)
-    if result:
+    print(str(result["database_exists"].item()))
+    if str(result["database_exists"].item()) != "0":
         LoadDatabase()
     else:
         CreateDatabase()
 
-def ReadDatabase():
-    pass
+def ReadIDbyPath(path):
+    sql_query = f'SELECT iID FROM images where ipath="{path}"'
+    result = pd.read_sql(sql_query, con=mydb)
+    return int(result["iID"].item())
+
+def imageMaxID():
+    sql_query = 'SELECT MAX(iID) as vali FROM images'
+    result = pd.read_sql(sql_query, con=mydb)
+    return result["vali"].item()
+
+def writeImage(id,path):
+    mycursor.execute(f'insert into images values({id}, "{path}") ON DUPLICATE KEY UPDATE ipath="{path}"')
+    mydb.commit()
+
+def writePixels(id,row,col,r,g,b):
+    mycursor.execute(f'insert into pixels values({id}, {row}, {col}, {r}, {g}, {b}) ON DUPLICATE KEY UPDATE r={r}, g={g}, r={b}')
+    mydb.commit()
+
+def writeSchemes(id,r,g,b,amount):
+    mycursor.execute(f'insert into schemes values({id}, {r}, {g}, {b}, {amount}) ON DUPLICATE KEY UPDATE amount={amount}')
+    mydb.commit()
 
 """Preperation"""
 def Path_generator():#directory = "path/to/directory"):
-    directory = "D:\Ablage\weather_image_recognition\dew"
-    for filename in os.listdir(directory):
-        # Check if the file is an image file
-        if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
-            # Open the image file
-            yield Image.open(os.path.join(directory, filename))
+    directory = "D:\Ablage\weather_image_recognition"
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            # Check if the file is an image file
+            if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+                # Open the image file
+                yield os.path.join(root, filename)
+
 
 def Get_color_scheme(vectors):
-    color_scheme1 = []
-    count_scheme1 = []
-    color_scheme2 = []
-    count_scheme2 = []
-    
-    # drop doubles
-    for rgb in tqdm(vectors):
-        if rgb not in color_scheme1:
-            color_scheme1.append(rgb)
-            count_scheme1.append(1)
-        else:
-            count_scheme1[color_scheme1.index(rgb)] += 1
-    for rgb in tqdm(color_scheme1):
-        count_value = count_scheme1[color_scheme1.index(rgb)]
-        converted_rgb = []
-        for value in rgb:
-            val_rest = value % 5
-            if val_rest >= 3:
-                converted_rgb.append(value + (5 - val_rest)) 
-            else:
-                converted_rgb.append(value - val_rest)
-        if converted_rgb not in color_scheme2:
-            color_scheme2.append(converted_rgb)
-            count_scheme2.append(count_value)
-        else:
-            count_scheme2[color_scheme2.index(converted_rgb)] += count_value
-    return color_scheme2, count_scheme2
-
-def Get_color_scheme_test(vectors):
     vectors = np.array(vectors)
     unique_vectors, vector_counts = np.unique(vectors, axis=0, return_counts=True)
     
@@ -91,46 +87,12 @@ def Get_color_scheme_test(vectors):
     
     unique_converted, converted_counts = np.unique(converted_vectors, axis=0, return_counts=True)
     
-    return unique_converted.tolist(), converted_counts.tolist()
-
-# def get_color_scheme_numpy(image):
-#     # Get the pixel data from the image
-#     pixels = np.array(image)
-
-#     # Flatten the pixel data to a 2D array of shape (N, 3), where N is the total number of pixels
-#     pixels_flat = pixels.reshape(-1, 3)
-
-#     # Calculate the unique colors and their counts
-#     unique_colors, color_counts = np.unique(pixels_flat, axis=0, return_counts=True)
-    
-#     color_scheme1 = list(unique_colors)
-#     count_scheme1 = list(color_counts)
-#     color_scheme2 = []
-#     count_scheme2 = []
-
-#     # Print the color counts
-#     #for color, count in zip(unique_colors, color_counts):
-#      #   print(f"RGB: {color}, Count: {count}")
-#     for rgb in tqdm(color_scheme1):
-#         count_value = count_scheme1[color_scheme1.index(rgb)]
-#         converted_rgb = []
-#         for value in rgb:
-#             val_rest = value % 5
-#             if val_rest >= 3:
-#                 converted_rgb.append(value + (5 - val_rest)) 
-#             else:
-#                 converted_rgb.append(value - val_rest)
-#         if converted_rgb not in color_scheme2:
-#             color_scheme2.append(converted_rgb)
-#             count_scheme2.append(count_value)
-#         else:
-#             count_scheme2[color_scheme2.index(converted_rgb)] += count_value
-#     return color_scheme2, count_scheme2
+    return unique_converted, converted_counts
 
 def GoogleAPI():
     pass
 
-def Image_to_rgb(image):
+def Image_to_rgb_scheme(image):
     # Convert the image to RGB mode if it's not already in RGB mode
     image = image.convert("RGB")
     
@@ -151,6 +113,23 @@ def Image_to_rgb(image):
     # return the list of vectors
     return vectors
 
+def Image_to_rgb_image(image, image_id):
+    # Convert the image to RGB mode if it's not already in RGB mode
+    image = image.convert("RGB")
+    
+    # Get the width and height of the image
+    width, height = image.size
+    
+    # Loop through each pixel in the image
+    for x in range(width):
+        for y in range(height):
+            # Get the RGB values of the pixel
+            r, g, b = image.getpixel((x, y))
+            # Append the RGB values to the list as a vector
+            # korrekturbedarf
+            print(image_id, x, y, r, g, b)
+            writePixels(image_id, x, y, r, g, b)
+
 def PreparationCNN():
     pass
 
@@ -161,13 +140,38 @@ def Full_Preperation():
     # Loop through each file in the directory
     image_generator = Path_generator()
 
+    maxID = imageMaxID()
+    counter = 0
+
+    if maxID is None:
+        print("all good")
+    else:
+        counter = int(maxID)
+
     # Open the image file
-    for image in image_generator:
-        vectors = Image_to_rgb(image)
+    for image_path in image_generator:
+        counter += 1
+
+        print(image_path)
+        writeImage(str(counter), image_path)
+
+        image_id = ReadIDbyPath(image_path)
+
+        image = Image.open(image_path)
+        vectors = Image_to_rgb_scheme(image)
+
         color_scheme, count_scheme = Get_color_scheme(vectors)
-        color_schemet, count_schemet = Get_color_scheme_test(vectors)
         #color_schemenp, count_schemenp = get_color_scheme_numpy(vectors)
-        print(np.setdiff1d(np.array([color_scheme, count_scheme]), np.array([color_schemet, count_schemet]))) 
+        for cols, couns in zip(color_scheme, count_scheme):
+            print(image_id, cols[0], cols[1], cols[2], couns)
+            writeSchemes(image_id, cols[0], cols[1], cols[2], couns)
+        #print(color_scheme, "\n", count_scheme)
+         
+        print(len(vectors))
+        image = image.resize((400,400))
+        Image_to_rgb_image(image, image_id)
+        print(400*400)
+        
         print("done next")
         break
 
@@ -187,4 +191,14 @@ def Predict():
 def Output():
     pass
 
-Full_Preperation()
+def myMain():
+    CheckDatabase()
+    Full_Preperation()
+
+
+
+print("Start")
+myMain()
+print("end")
+mycursor.close()
+mydb.close()
