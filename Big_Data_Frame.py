a17a85ff9ea66c95f7de03b08e9fa0d126c0b56f
bro-tec <big0brudi@gmail.com>
2023-06-22 11:42:10 +0200

    corrected errors

--- Big_Data_Frame.py
+++ Big_Data_Frame.py
@@ -28,7 +28,7 @@ def CreateDatabase():
     mycursor.execute("create database bigData")
     mycursor.execute("use bigData")
     mycursor.execute("create table images(iID int primary key not null, ipath varchar(120) unique)")
-    mycursor.execute("create table pixels(pID int, prow int(100), pcol int(100), r int(3), g int(3), b int(3), primary key(pID, prow, pcol), foreign key (pID) references images(iID))")
+    mycursor.execute("create table embeddings(eID int, erow int(100), ecol int(100), r int(3), g int(3), b int(3), primary key(eID, erow, ecol), foreign key (eID) references images(iID))")
     mycursor.execute("create table schemes(sID int, r int(3), g int(3), b int(3), amount int(10), primary key(sID, r, g, b), foreign key (sID) references images(iID))")
     mydb.commit()
 
@@ -62,7 +62,7 @@ def writeImage(id,path):
     mydb.commit()
 
 def writePixels(id,row,col,r,g,b):
-    mycursor.execute(f'insert into pixels values({id}, {row}, {col}, {r}, {g}, {b}) ON DUPLICATE KEY UPDATE r={r}, g={g}, r={b}')
+    mycursor.execute(f'insert into embeddings values({id}, {row}, {col}, {r}, {g}, {b}) ON DUPLICATE KEY UPDATE r={r}, g={g}, r={b}')
     mydb.commit()
 
 def writeSchemes(id,r,g,b,amount):
@@ -71,7 +71,7 @@ def writeSchemes(id,r,g,b,amount):
 
 """Preperation"""
 def Path_generator():#directory = "path/to/directory"):
-    directory = "D:\Ablage\weather_image_recognition"
+    directory = "C:\\Users\\Shazil Khan\\Pictures\\bigdata\\weather_image_recognition"
     for root, dirs, files in os.walk(directory):
         for filename in files:
             # Check if the file is an image file
@@ -84,7 +84,7 @@ def Get_color_scheme(vectors):
     vectors = np.array(vectors)
     unique_vectors, vector_counts = np.unique(vectors, axis=0, return_counts=True)
     
-    converted_vectors = np.where((unique_vectors % 5) >= 3, unique_vectors + (5 - (unique_vectors % 5)), unique_vectors - (unique_vectors % 5))
+    converted_vectors = np.where((unique_vectors % 15) >= 3, unique_vectors + (15 - (unique_vectors % 15)), unique_vectors - (unique_vectors % 15))
     
     unique_converted, converted_counts = np.unique(converted_vectors, axis=0, return_counts=True)
     
@@ -99,7 +99,8 @@ def Image_to_rgb_scheme(image):
     
     # Get the width and height of the image
     width, height = image.size
-    im = np.array(image)
+    im3d = np.array(image)
+    im = im3d.reshape(-1, im3d.shape[-1])
     return im
 
 def Image_to_rgb_image(image, image_id):
@@ -149,17 +150,19 @@ def Full_Preperation():
         image = Image.open(image_path)
         vectors = Image_to_rgb_scheme(image)
 
+        print(vectors)
+
         color_scheme, count_scheme = Get_color_scheme(vectors)
         #color_schemenp, count_schemenp = get_color_scheme_numpy(vectors)
+        #print(color_scheme, count_scheme)
         for cols, couns in zip(color_scheme, count_scheme):
             print(image_id, cols[0], cols[1], cols[2], couns)
             writeSchemes(image_id, cols[0], cols[1], cols[2], couns)
         #print(color_scheme, "\n", count_scheme)
-         
-        print(len(vectors))
-        image = image.resize((400,400))
-        Image_to_rgb_image(image, image_id)
-        print(400*400)
+        #print(len(vectors))
+        #image = image.resize((400,400))
+        #Image_to_rgb_image(image, image_id)
+        #print(400*400)
         
 """Code"""
 def Input():
