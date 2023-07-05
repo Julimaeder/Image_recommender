2a6df4bc263dc327f2c2688922686035887ab2f3
bro-tec <ig0brudi@gmail.com>
2023-07-05 21:28:16 +0200

    made code faster and corrected database to save 100.000 or more data

--- Big_Data_Frame.py
+++ Big_Data_Frame.py
@@ -8,11 +8,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import sqlite3
+import datetime
 #from sqlalchemy import create_engine
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-sqlPath = "../databases/my_database.db"
-imagesPath = "D:\\Ablage\\weather_image_recognition"
+sqlPath = "../databases/my_database1.db"
+imagesPath = "E:\\"
 predictPath = "D:\\Ablage\\predict"
 
 mydb = sqlite3.connect(sqlPath)
@@ -38,14 +39,19 @@ def CreateDatabase():
     mydb.commit()
     #mycursor.execute("CREATE TABLE IF NOT EXISTS embeddings(eID INT, erow INT, ecol INT, r INT, g INT, b INT, PRIMARY KEY(eID, erow, ecol), FOREIGN KEY (eID) REFERENCES images(iID));")
     #mydb.commit()
-    mycursor.execute("CREATE TABLE IF NOT EXISTS schemes(sID INT, r INT, g INT, b INT, amount INT, PRIMARY KEY(sID, r, g, b), FOREIGN KEY (sID) REFERENCES images(iID));")
+    mycursor.execute("CREATE TABLE IF NOT EXISTS schemes(sID INT, row INT, PRIMARY KEY(sID, row), FOREIGN KEY (sID) REFERENCES images(iID));")
+    mydb.commit()
+    anzahl_spalten = 675
+    for spalten_nr in range(0, anzahl_spalten):
+        spaltenname = f"spalte_{spalten_nr}"
+        mycursor.execute(f"ALTER TABLE schemes ADD {spaltenname} int")
     mydb.commit()
     
 
 
 def CheckDatabase():
     # check if the Database is implemented
-    sql_query = f'SELECT name FROM sqlite_master WHERE name="schemes_distances"'
+    sql_query = f'SELECT name FROM sqlite_master WHERE name="schemes"'
     result = pd.read_sql(sql_query, con=mydb)
     if len(result['name']) == 0:
         CreateDatabase()
@@ -81,7 +87,7 @@ def ReadALLID():
         return None
     
 def ReadSchemesbyID(id):
-    sql_query = f'SELECT r, g, b, amount FROM schemes where sID="{id}"'
+    sql_query = f'SELECT * FROM schemes where sID="{id}"'
     result = pd.read_sql(sql_query, con=mydb)
     if len(result["amount"]) > 0:
         return result
@@ -100,17 +106,8 @@ def writeImage(id,path):
     mycursor.execute(f'insert into images values({id}, "{path}")')
     mydb.commit()
 
-def writePixels(id,row,col,r,g,b):
-    mycursor.execute(f'insert into embeddings values({id}, {row}, {col}, {r}, {g}, {b})')
-    mydb.commit()
-
 def writeSchemes(id,r,g,b,amount):
     mycursor.execute(f'insert into schemes values({id}, {r}, {g}, {b}, {amount})')
-    mydb.commit()
-
-def writeDistances(id1,id2,e,m,c,j,h):
-    #print(f'insert into schemes values({id1}, {id2}, {e}, {m}, {c}, {j}, {h}) ON DUPLICATE KEY UPDATE Euclidean={e}, Manhattan={m}, Cosine={c}, Jaccard={j}, Hamming={h}')
-    mycursor.execute(f'insert into schemes_distances values({id1}, {id2}, {e}, {m}, {c}, {j}, {h}) ON DUPLICATE KEY UPDATE Euclidean={e}, Manhattan={m}, Cosine={c}, Jaccard={j}, Hamming={h}')
     mydb.commit()
 
 """Preperation"""
@@ -126,13 +123,17 @@ def Path_generator(ipath):#directory = "path/to/directory"):
 
 def Get_color_scheme(vectors):
     vectors = np.array(vectors)
-    unique_vectors, vector_counts = np.unique(vectors, axis=0, return_counts=True)
-    
-    converted_vectors = np.where((unique_vectors % 15) >= 3, unique_vectors + (15 - (unique_vectors % 15)), unique_vectors - (unique_vectors % 15))
-    
-    unique_converted, converted_counts = np.unique(converted_vectors, axis=0, return_counts=True)
-    
-    return unique_converted, converted_counts
+    vector_color, vector_counts = np.unique(vectors, axis=0, return_counts=True)
+    u_vectors = vector_color - (vector_color%17)
+    unique_vectors, unique_counts = np.unique(u_vectors, axis=0, return_counts=True)
+    count = 0
+    numbers = np.zeros(len(unique_counts), dtype=np.int64)
+
+    for i, c in enumerate(unique_counts):
+        numbers[i] = np.sum(vector_counts[count:count+c])
+        count += c
+    uvs = unique_vectors / 17
+    return uvs, numbers
 
 def GoogleAPI():
     pass
@@ -174,6 +175,7 @@ def Full_Preperation():
     # Loop through each file in the directory
     image_generator = Path_generator(imagesPath)
 
+    print("hi")
     maxID = imageMaxID()
     counter = 0
 
@@ -189,53 +191,58 @@ def Full_Preperation():
         image_id = ReadIDbyPath(image_path)
         if image_id is None:
             counter += 1
-
+            print(counter)
             writeImage(counter, image_path)
-            try:
-                image = Image.open(image_path)
-                vectors = Image_to_rgb_scheme(image)
+            #try:
+            image = Image.open(image_path)
+            vectors = Image_to_rgb_scheme(image)
 
-                color_scheme, count_scheme = Get_color_scheme(vectors)
-                
-                df = pd.DataFrame({
-                    'sID': counter,
-                    'r': [cols[0] for cols in color_scheme],
-                    'g': [cols[1] for cols in color_scheme],
-                    'b': [cols[2] for cols in color_scheme],
-                    'amount': count_scheme
-                })
-                print(counter, color_scheme.shape, len(count_scheme))
-                
-                #print("\n", [cols for cols in count_scheme])
+            color_scheme, count_scheme = Get_color_scheme(vectors)
+            print(color_scheme.shape, count_scheme.shape)
 
-                # Insert the data into the database
-                df.to_sql(name='schemes', con=mydb, if_exists='append',index = False, chunksize = 1000)
-            except:
-                print("continue")
+            rgb_values = np.zeros((16, 16, 16))
+            rgb_values[color_scheme[:, 0].astype('int32'), color_scheme[:, 1].astype('int32'), color_scheme[:, 2].astype('int32')] = count_scheme
+
+            num = rgb_values.transpose(2,0,1).reshape((8,-1)).astype('int32')
+            #print(num.shape)
+
+            anzahl_spalten = 512
+
+            df = pd.DataFrame({'sID': image_id, "row": range(8)})
+
+            column_data = pd.DataFrame(num, columns=[f"spalte_{spalten_nr}" for spalten_nr in range(anzahl_spalten)])
+
+            # Merge the two DataFrames based on index
+            df = df.merge(column_data, left_index=True, right_index=True)     
+
+            df = df[df.iloc[:, 2:].sum(axis=1) != 0]
+            #print(df.shape)
+
+            # Insert the data into the database
+            df.to_sql(name='schemes', con=mydb, if_exists='append',index = False, chunksize = 1000)
+            #except :
+                #print("continue")
             #print(color_scheme, "\n", count_scheme)
-        #print(len(vectors))
-        #image = image.resize((400,400))
-        #Image_to_rgb_image(image, image_id)
-        #print(400*400)
 
 def predictschemes_gen(image):
     vectors = Image_to_rgb_scheme(image)
 
     color_scheme, count_scheme = Get_color_scheme(vectors)
-    np_i = np.zeros((18, 18, 18))
-    for [r, g, b], a in zip(color_scheme, count_scheme):
-        np_i[int(r/15)][int(g/15)][int(b/15)] = int(a)
-    np_i = np_i.reshape(-1)
+    rgb_values = np.zeros((16, 16, 16))
+    rgb_values[color_scheme[:, 0].astype('int32'), color_scheme[:, 1].astype('int32'), color_scheme[:, 2].astype('int32')] = count_scheme
+
+    num = rgb_values.flatten().astype('int32')
+
+    np_i = num.reshape(-1).tolist()
+
     #print(color_scheme.shape, len(count_scheme))
     ids = ReadALLID()
     for j in ids:
         df_j = ReadSchemesbyID(j)
         if df_j is not None:
-            np_j = np.zeros((18, 18, 18))
-            for x , [r, g, b, a] in df_j.iterrows():
-                np_j[int(r/15)][int(g/15)][int(b/15)] = int(a)
+            df_j.values.tolist()
             #print(i,j)
-            np_j = np_j.reshape(-1)
+            np_j = df.values.reshape(-1,).tolist()
             e = Distances(j, np_i,np_j)
             yield j, e 
 
@@ -322,10 +329,18 @@ def Full_Prediction():
 
 
 
+start = datetime.datetime.now()
+print("\nStart:", start, ", time:")
 
 print("Start")
-#Full_Preperation()
-Full_Prediction()
+Full_Preperation()
+#Full_Prediction()
 print("end")
+
+end = datetime.datetime.now()
+duration = end - start
+
+print("\nStart:", start, ", end:", end, ", duration:", duration)
+
 mycursor.close()
 mydb.close()
