99d24e139ca6380fed44f9c46e3a2f88ebb4778c
bro-tec <ig0brudi@gmail.com>
2023-07-08 12:13:35 +0200

    updated tests

--- Scripts/test_image_recommender.py
+++ Scripts/test_image_recommender.py
@@ -21,8 +21,8 @@ def test_image_recommender():
     #Test scheme:
     path = "../.github/Test_bild/standing-german-shepherd.jpg"
     expected_output_scheme = ['E:\\images\\weather_image_recognition\\dew\\2208.jpg', 1, 9315, 0, 0, 0, 0, 0, 31859, 0, 0, 0, 0, 0, 3307, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2229, 2, 0, 0, 0, 0, 34805, 17, 0, 0, 0, 0, 2626, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 327, 43, 0, 0, 0, 0, 3735, 277, 21, 0, 0, 0, 50, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 40, 297, 96, 1, 0, 0, 124, 85, 174, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 14, 21, 92, 132, 56, 5, 1, 5, 11, 5, 60, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 11, 15, 0, 1, 2, 6, 53, 25]
-    c = [f"spalte_{i}" for i in range(len(expected_output_scheme))]
-    df = pd.DataFrame([expected_output_scheme], columns=c)
+    c = ["iID", "sID"] + [f"spalte_{i}" for i in range(len(expected_output_scheme))]
+    df = pd.DataFrame([1,1,expected_output_scheme], columns=c)
     output_path = Full_Prediction(path, df)
     # image = Image.open(path)
     # vectors = Image_to_rgb_scheme(image)
