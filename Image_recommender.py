369082eb8661822d7c897d555022fb008f36e1b1
Julimaeder <maeder.julian@gmail.com>
2023-07-08 18:24:58 +0200

    final push dieser Datei

--- Image_recommender.py
+++ Image_recommender.py
@@ -1,7 +1,11 @@
+"""
+---Image recommender---
+Bitte vorher die Readme lesen
+"""
 import pathlib, os
 filepath = str(pathlib.Path(__file__).parent.resolve())
 filepath2 = os.path.join(filepath, 'Scripts')
 os.chdir(filepath2)
 from Image_recommender_Predictions import main
 
-main("Path/to/image.jpg")
+main("path/to/folder")
