7f9b672ca168dc62c422b048199f5964119181a1
bro-tec <ig0brudi@gmail.com>
2023-07-08 11:17:45 +0200

    updated tests

--- Scripts/test_image_recommender.py
+++ Scripts/test_image_recommender.py
@@ -1,8 +1,9 @@
 from Scripts.Image_recommender_Predictions import real_image_label, extract_image_embeddings
 from Scripts.Image_recommender_Vorverarbeitung import load_and_compress_image
-from Scripts.Color_Scheme import Image_to_rgb_scheme, Get_color_scheme
+from Scripts.Color_Scheme import Image_to_rgb_scheme, Get_color_scheme, Full_Prediction
 from PIL import Image
 import numpy as np
+import pandas as pd
 
 def test_image_recommender():
     path = "../.github/Test_bild/standing-german-shepherd.jpg"
@@ -18,15 +19,18 @@ def test_image_recommender():
     assert output_embeddings == expected_output_embeddings
 
     #Test scheme:
-    expected_output_scheme = [5500, 595, 0, 0, 0, 0, 140, 88, 0, 0]
-    image = Image.open(path)
-    vectors = Image_to_rgb_scheme(image)
-    color_scheme, count_scheme = Get_color_scheme(vectors)
-    rgb_values = np.zeros((6, 6, 6), dtype=int)
-    rgb_values[color_scheme[:, 0].astype('int32'), color_scheme[:, 1].astype('int32'), color_scheme[:, 2].astype('int32')] = count_scheme
-    np_i = rgb_values.flatten().astype('int32')
-    output_scheme = list(np_i[:10])
-    assert output_scheme == expected_output_scheme
+    expected_output_scheme = ['E:\\images\\weather_image_recognition\\dew\\2208.jpg', 1, 9315, 0, 0, 0, 0, 0, 31859, 0, 0, 0, 0, 0, 3307, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2229, 2, 0, 0, 0, 0, 34805, 17, 0, 0, 0, 0, 2626, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 327, 43, 0, 0, 0, 0, 3735, 277, 21, 0, 0, 0, 50, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 40, 297, 96, 1, 0, 0, 124, 85, 174, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 14, 21, 92, 132, 56, 5, 1, 5, 11, 5, 60, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 11, 15, 0, 1, 2, 6, 53, 25]
+    c = [f"spalte_{i}" for i in range(len(expected_output_scheme))]
+    df = pd.DataFrame([expected_output_scheme], columns=c)
+    output_path = Full_Prediction(path, df)
+    # image = Image.open(path)
+    # vectors = Image_to_rgb_scheme(image)
+    # color_scheme, count_scheme = Get_color_scheme(vectors)
+    # rgb_values = np.zeros((6, 6, 6), dtype=int)
+    # rgb_values[color_scheme[:, 0].astype('int32'), color_scheme[:, 1].astype('int32'), color_scheme[:, 2].astype('int32')] = count_scheme
+    # np_i = rgb_values.flatten().astype('int32')
+    # output_scheme = list(np_i[:10])
+    assert output_path == expected_output_scheme[0]
     
  #   #Test Daten auslesen aus der Datenbank
  #   expected_output_db = 'E:\\images\\coco2017_unlabeled\\unlabeled2017\\000000075102.jpg'
