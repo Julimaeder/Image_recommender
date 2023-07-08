558868c5510000e1377d5a98b11a0ca63a90795a
bro-tec <ig0brudi@gmail.com>
2023-07-08 11:26:58 +0200

    updated tests

--- Scripts/test_image_recommender.py
+++ Scripts/test_image_recommender.py
@@ -4,10 +4,10 @@ from Scripts.Color_Scheme import Image_to_rgb_scheme, Get_color_scheme, Full_Pre
 from PIL import Image
 import numpy as np
 import pandas as pd
-path = "../.github/Test_bild/standing-german-shepherd.jpg"
 
 def test_image_recommender():
     #Test Label:
+    path = "../.github/Test_bild/standing-german-shepherd.jpg"
     expected_output_label = 'German_shepherd' #genaue Werte können nur schelcht verglichen werden, da die Berechnungen teils minimal von einander abweichen
     image = load_and_compress_image(path, target_size=(224, 224))
     output_label = real_image_label(path,image)[1]
@@ -19,6 +19,7 @@ def test_image_recommender():
     assert output_embeddings == expected_output_embeddings
 
     #Test scheme:
+    path = "../.github/Test_bild/standing-german-shepherd.jpg"
     expected_output_scheme = ['E:\\images\\weather_image_recognition\\dew\\2208.jpg', 1, 9315, 0, 0, 0, 0, 0, 31859, 0, 0, 0, 0, 0, 3307, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2229, 2, 0, 0, 0, 0, 34805, 17, 0, 0, 0, 0, 2626, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 327, 43, 0, 0, 0, 0, 3735, 277, 21, 0, 0, 0, 50, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 40, 297, 96, 1, 0, 0, 124, 85, 174, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 14, 21, 92, 132, 56, 5, 1, 5, 11, 5, 60, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 11, 15, 0, 1, 2, 6, 53, 25]
     c = [f"spalte_{i}" for i in range(len(expected_output_scheme))]
     df = pd.DataFrame([expected_output_scheme], columns=c)
