049d222375cdb0fe0228e4fefd74a0e2cce5a483
bro-tec <ig0brudi@gmail.com>
2023-07-08 03:18:41 +0200

    correct commit for clean code

--- Scripts/Color_Scheme.py
+++ Scripts/Color_Scheme.py
@@ -5,15 +5,8 @@ import pandas as pd
 from tqdm import tqdm
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import sqlite3
-import time
-import datetime
 
-#warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
-#warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
-#from sqlalchemy import create_engine
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 sqlPath = "./Data/databases/my_database.db"
@@ -26,8 +19,6 @@ def CreateDatabase(mydb,mycursor):
     # just execute it if the Database isn't implemented
     mycursor.execute("CREATE TABLE IF NOT EXISTS images(iID INT PRIMARY KEY NOT NULL, ipath VARCHAR(120) UNIQUE);")
     mydb.commit()
-    #mycursor.execute("CREATE TABLE IF NOT EXISTS embeddings(eID INT, erow INT, ecol INT, r INT, g INT, b INT, PRIMARY KEY(eID, erow, ecol), FOREIGN KEY (eID) REFERENCES images(iID));")
-    #mydb.commit()
     mycursor.execute("CREATE TABLE IF NOT EXISTS schemes(sID INT PRIMARY KEY, FOREIGN KEY (sID) REFERENCES images(iID));")
     mydb.commit()
     anzahl_spalten = 216
@@ -54,8 +45,6 @@ def CheckDatabase():
 
 
 CheckDatabase()
-
-#engine = create_engine('mysql+mysqldb://bigData:Informatiker1@localhost/root', echo = False)
 
 def writeSchemes(df):
     mydb = sqlite3.connect(sqlPath)
@@ -148,7 +137,7 @@ def writeImage(id,path):
 
 
 """Preperation"""
-def Path_generator(ipath):#directory = "path/to/directory"):
+def Path_generator(ipath):
     directory = ipath
     for root, dirs, files in os.walk(directory):
         for filename in files:
@@ -167,20 +156,6 @@ def Get_color_scheme(vectors):
     uvs = unique_vectors / 51
 
     return uvs, unique_counts
-
-#def Get_color_scheme(vectors):
-#     vectors = np.array(vectors)
-#     vector_color, vector_counts = np.unique(vectors, axis=0, return_counts=True)
-#     u_vectors = vector_color - (vector_color%51)
-#     unique_vectors, unique_counts = np.unique(u_vectors, axis=0, return_counts=True)
-#     count = 0
-#     numbers = np.zeros(len(unique_counts), dtype=np.int64)
-
-#     for i, c in enumerate(unique_counts):
-#         numbers[i] = np.sum(vector_counts[count:count+c])
-#         count += c
-#     uvs = unique_vectors / 51
-#     return uvs, numbers
 
 
 def Image_to_rgb_scheme(image):
@@ -218,34 +193,20 @@ def Full_Preperation():
 
         image_id = ReadIDbyPath(image_path)
         if image_id is None:
-            #t = time.time()
             counter += 1
-            #print(counter)
-            
-            #time1 = time.time()
             writeImage(counter, image_path)
-            #try:
             image = Image.open(image_path)
 
             vectors = Image_to_rgb_scheme(image)
             
-            #print(vectors.shape)
-            
-            #time2 = time.time()
             color_scheme, count_scheme = Get_color_scheme(vectors)
-            #print(color_scheme.shape, count_scheme.shape)
 
-            
-            #time3 = time.time()
             rgb_values = np.zeros((6, 6, 6))
             rgb_values[color_scheme[:, 0].astype('int32'), color_scheme[:, 1].astype('int32'), color_scheme[:, 2].astype('int32')] = count_scheme
             
             num = rgb_values.flatten().astype('int32')#time4 = time.time()
             
             values = num.tolist() 
-            #num = rgb_values.transpose(2,0,1).reshape((8,-1)).astype('int32')
-            #print(num.shape)
-
             anzahl_spalten = 216
 
             #df = pd.DataFrame({'sID': counter})
@@ -253,29 +214,9 @@ def Full_Preperation():
             data = [counter] + values
             df = pd.DataFrame(columns=cols)
             df.loc[0] = data
-            #time5 = time.time()
-            # Merge the two DataFrames based on index
-            #df = df.merge(column_data, left_index=True, right_index=True)     
 
-            #df = df[df.iloc[:, 2:].sum(axis=1) != 0]
-            #print(df.shape)
-            
-
-            #time6 = time.time()
             # Insert the data into the database
             writeSchemes(df)
-                     
-            #print("time: ", time.time()-t)
-            #time7 = time.time()   
-            #print("1-2:", time2-time1)
-            #print("2-3:", time3-time2)
-            #print("3-4:", time4-time3)
-            #print("4-5:", time5-time4)
-            #print("5-6:", time6-time5)
-            #print("7-6:", time7-time6)
-            #except :
-                #print("continue")
-            #print(color_scheme, "\n", count_scheme)
 
 def predictschemes_gen(image):
     vectors = Image_to_rgb_scheme(image)
@@ -291,7 +232,6 @@ def predictschemes_gen(image):
         df_j = ReadSchemesbyID(j)
         if df_j is not None:
             df_j.values.tolist()
-            #print(i,j)
             np_j = df_j.iloc[:, 1:].values.tolist()
             e = Distances(j, np_i,np_j)
             yield j, e 
@@ -300,7 +240,6 @@ def predictschemes_gen(image):
 """Code"""
 def Distances(image, df1, num_images=5):
     distances = []
-    #nearest_images = []
     for i in tqdm(range(len(df1))):
         colors = df1.iloc[i].values[1:]
         distance = np.linalg.norm(image - colors)
@@ -320,7 +259,6 @@ def Full_Prediction(image_path, df):
     # Open and load all images
     image = Image.open(image_path)
         
-    #schemes = predictschemes_gen(image)
     vectors = Image_to_rgb_scheme(image)
 
     color_scheme, count_scheme = Get_color_scheme(vectors)
@@ -328,33 +266,10 @@ def Full_Prediction(image_path, df):
     rgb_values[color_scheme[:, 0].astype('int32'), color_scheme[:, 1].astype('int32'), color_scheme[:, 2].astype('int32')] = count_scheme
         
     np_i = rgb_values.flatten().astype('int32')
-    #print(np_i.shape, np_i)
     color_scheme_distances = Distances(np_i, df)
 
-    #print(type(color_scheme_distances), "\n","\n",color_scheme_distances)
     smallest_list = []
     for path in color_scheme_distances["path"]:
         smallest_list.append(path)
     
     return smallest_list
-
-
-
-
-# start = datetime.datetime.now()
-# print("\nStart:", start, ", time:")
-
-# print("Start")
-# #Full_Preperation()
-# Full_Prediction()
-# print("end")
-
-# end = datetime.datetime.now()
-# duration = end - start
-
-# print("\nStart:", start, ", end:", end, ", duration:", duration)
-
-# mycursor.close()
-# mydb.close()
-
-# speichern als lost(filename,pfad) x 5
